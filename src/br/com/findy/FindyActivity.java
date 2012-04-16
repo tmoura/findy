aec6dcbfea1f5ef91c19cb544ea394cef6480e1b
Thiago Moura <tmoura@gmail.com>
2012-04-15 22:13:38 -0300

    Primeiro Commit em 15/04/2012

---
+++ src/br/com/findy/FindyActivity.java
@@ -0,0 +1,13 @@
+package br.com.findy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
